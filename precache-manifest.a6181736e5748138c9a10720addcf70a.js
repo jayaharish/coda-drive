ee90db5127e6c86b6a8fe6ffca1a89a092e8dd5f
jayaharish <61152197+jayaharish@users.noreply.github.com>
2020-07-11 14:21:45 +0530

    Updates

---
+++ precache-manifest.a6181736e5748138c9a10720addcf70a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b10beba0312ee20082ebe32dfca3220",
+    "url": "/coda-drive/index.html"
+  },
+  {
+    "revision": "ee9f7ae2fecd8df2dc9f",
+    "url": "/coda-drive/static/css/main.22733d34.chunk.css"
+  },
+  {
+    "revision": "baee086fbf34d31a12e7",
+    "url": "/coda-drive/static/js/2.d042e762.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/coda-drive/static/js/2.d042e762.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee9f7ae2fecd8df2dc9f",
+    "url": "/coda-drive/static/js/main.75136dce.chunk.js"
+  },
+  {
+    "revision": "564d6d3cd944bbdccd63",
+    "url": "/coda-drive/static/js/runtime-main.d497bd84.js"
+  },
+  {
+    "revision": "bb5557317677de08603590958e9ae595",
+    "url": "/coda-drive/static/media/product.bb555731.png"
+  }
+]);
